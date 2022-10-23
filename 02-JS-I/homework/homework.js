a76cbde71de0b813dde2bc1b351013780474f70f
ING Eduard Escobar <110680126+bugabek@users.noreply.github.com>
2022-10-23 13:25:01 +0200

    primer guard

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,7 +1,7 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = "";
+const nuevaString = "epale";
 
 // Crea una variable numérica, puede ser cualquier número:
 const nuevoNum = 5;
@@ -10,13 +10,13 @@ const nuevoNum = 5;
 const nuevoBool = true;
 
 // Resuelve el siguiente problema matemático:
-const nuevaResta = 10 - null === 5;
+const nuevaResta = 10 - null === 2;
 
 // Resuelve el siguiente problema matemático:
 const nuevaMultiplicacion = 10 * null === 40 ;
 
 // Resuelve el siguiente problema matemático:
-const nuevoModulo = 21 % 5 === null;
+const nuevoModulo = 21 % 5 === null; 1,05 ;
 
 
 // En los próximos 22 problemas, deberás completar la función.
@@ -35,6 +35,7 @@ function suma(x, y) {
   // "x" e "y" son números
   // Suma "x" e "y" juntos y devuelve el valor
   // Tu código:
+  
   
 }
 
